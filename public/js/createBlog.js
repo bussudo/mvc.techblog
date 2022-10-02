55faafd28db05462f2eb3ce01f2279962d0b8807
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-02 18:09:36 -0400

    working on creating blogs

--- public/js/createBlog.js
+++ public/js/createBlog.js
@@ -1,6 +1,8 @@
 // Create Blog
 // function from createBlog.handlebars
 
+// const { User } = require("../../models");
+
 // const router = require("../../controllers");
 // const { Blog } = require("../../models");
 
@@ -19,49 +21,9 @@ submitBtn.addEventListener("click", async function () {
   } else alert("Please try again");
 });
 
-// I think i need code here to add timestamp and create field for which user created this
+const timeStamp = new Date().toLocaleTimeString();
+const blogAuthor = ${firstName} &"" & ${lastName};
+// const name = User.fName + "" + User.lName
 
-// Edit Blog
-// function from dashboard.handlebars,
-// let editBtn = document.getElementById("edit-blog");
-
-// editBtn.addEventListener("click", async function () {
-//   let title = document.getElementById("titleblog").value;
-//   let contents = document.getElementById("contentblog").value;
-//   let response = await fetch("/api/blogs", {
-//     method: "PUT",
-//     body: JSON.stringify({ title, contents }),
-//     headers: { "Content-Type": "application/json" },
-//   });
-//   if (response.ok) {
-//     document.location.replace("/dashboard");
-//   } else alert("Please try again");
-// });
-
-// Update Blog
-// let updBtn = document.getElementById("update-blog");
-
-// updBtn.addEventListener("click", async function () {
-//   let title = document.getElementById("titleBlog").value;
-//   let contents = document.getElementById("contentBlog").value;
-//   let response = await fetch("/api/blogs", {
-//     method: "PuT",
-//     body: JSON.stringify({ title, contents }),
-//     headers: { "Content-Type": "application/json" },
-//   });
-//   if (response.ok) {
-//     document.location.replace("/dashboard");
-//   } else alert("Please try again");
-// });
-
-// Delete Blog
-// let deleteBtn = document.getElementById("delete-blog").value;
-
-// let id = deleteBtn.addEventListener("click", async function () {
-//   let response = await fetch(`/api/blogs/${id}`, {
-//     method: "DELETE",
-//   });
-//   if (response.ok) {
-//     document.location.replace("/dashboard");
-//   } else alert("Please try again");
-// });
+console.log(timeStamp);
+console.log(blogAuthor);
