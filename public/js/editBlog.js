449609a6a3b8895569bb358952640889e66b8d8a
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-02 14:58:08 -0400

    now working on Edit function

--- public/js/editBlog.js
+++ public/js/editBlog.js
@@ -2,10 +2,12 @@
 // function from dashboard.handlebars,
 let editBtn = document.getElementById("edit-blog");
 
-editBtn.addEventListener("click", async function () {
+editBtn.addEventListener("click", async function (event) {
   let title = document.getElementById("titleblog").value;
   let contents = document.getElementById("contentblog").value;
-  let response = await fetch("/api/blogs", {
+  let id = this.classList.value;
+
+  let response = await fetch(`/api/blogs/${id}`, {
     method: "PUT",
     body: JSON.stringify({ title, contents }),
     headers: { "Content-Type": "application/json" },
@@ -14,22 +16,6 @@ editBtn.addEventListener("click", async function () {
     document.location.replace("/dashboard");
   } else alert("Please try again");
 });
-
-// Update Blog
-// let updBtn = document.getElementById("update-blog");
-
-// updBtn.addEventListener("click", async function () {
-//   let title = document.getElementById("titleBlog").value;
-//   let contents = document.getElementById("contentBlog").value;
-//   let response = await fetch("/api/blogs", {
-//     method: "PuT",
-//     body: JSON.stringify({ title, contents }),
-//     headers: { "Content-Type": "application/json" },
-//   });
-//   if (response.ok) {
-//     document.location.replace("/dashboard");
-//   } else alert("Please try again");
-// });
 
 // Delete Blog
 
