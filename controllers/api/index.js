c1d7ee63c19e70db7f2498037a8bacb82f99f50c
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-07 12:22:54 -0400

    made changes to routes and handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const blogRoutes = require("./blog-routes");
 const userRoutes = require("./user-routes");
-const commentRoutes = require("./comment-routes");
+// const commentRoutes = require("./comment-routes");
 
 router.use("/users", userRoutes);
 router.use("/blogs", blogRoutes);
-router.use("/comments", comments);
+// router.use("/comments", comments);
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 // Import express-session
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const Pusher = require("pusher");
+// const Pusher = require("pusher");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const helpers = require("./utils/helpers");
