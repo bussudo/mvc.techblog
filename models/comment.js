9148892e9dba95f746e8626f3791fbee1b03b176
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-07 09:25:46 -0400

    trying to resolve routing error

--- models/comment.js
+++ models/comment.js
@@ -1,30 +1,45 @@
 const { Model, DataTypes } = require("sequelize");
-const sequelize = require("..config/connection");
-class Comment extends Blog {}
-Comment.init({
-  user_id: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    references: {
-      model: "user",
-      key: "id",
+const sequelize = require("../config/connection");
+class Comment extends Model {}
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    blog_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "blog",
+        key: "id",
+      },
+    },
+    commentDate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: DataTypes.NOW,
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      require: false,
     },
   },
-  blog_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: "blog",
-      key: "id",
-    },
-  },
-  commentDate: {
-    type: DataTypes.DATE,
-    allowNull: true,
-    defaultValue: DataTypes.NOW,
-  },
-  comment: {
-    type: DataTypes.TEXT,
-    require: false,
-  },
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    modelName: "comment",
+  }
+);
+module.exports = Comment;
